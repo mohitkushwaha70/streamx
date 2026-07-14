f86bd450de93914362e93bc2c8eca40ea193b171
mohitkushwaha70 <mohitkushwaha70896@gmail.com>
2026-07-14 09:35:52 +0000

    Add avatar image upload: file picker, canvas resize, profile + navbar display

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: '7d', etag: true }));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 app.use(session({
   secret: process.env.SESSION_SECRET || 'streamx_secret_2026',
